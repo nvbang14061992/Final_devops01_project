323ab1da5219ce673d736700ae48886dc276e6ef
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2026-02-03 22:31:57 +0700

    replace new vps ip

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
   // baseURL:"http://localhost:8000/api"
-  baseURL: "http://206.189.32.225:8000/api",
+  baseURL: "http://188.166.255.62:8000/api",
 });
 export default instance;
